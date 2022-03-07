3b7bcb1b1836e7e4ed7d86af11c191ebcb3996e6
Iain McWhinnie <2566283M@student.gla.ac.uk>
2022-03-07 22:08:46 +0000

    Blank Models

--- venyou/venyou_app/models.py
+++ venyou/venyou_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    pass
+
+class Venue(models.Model):
+    pass
+
+class Rating(models.Model):
+    pass
+
+class Event(models.Model):
+    pass
