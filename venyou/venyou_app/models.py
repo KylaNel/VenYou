96dec6d44ee763e55bf8037a0141194173ef2176
Iain McWhinnie <2566283M@student.gla.ac.uk>
2022-03-07 22:15:55 +0000

    User Model

--- venyou/venyou_app/models.py
+++ venyou/venyou_app/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(models.Model):
-    pass
+class UserProfile(models.Model):
+    # To extend on the functionality of Django's user model
+    # we create a one-to-one link with the user
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    picture = models.ImageField(upload_to='', blank=True)
+    is_owner = models.BooleanField()
+
+    def __str__(self):
+        return self.user.username
 
 class Venue(models.Model):
     pass
