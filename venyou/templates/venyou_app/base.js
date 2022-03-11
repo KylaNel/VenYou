effe0bbc8cbb3396aec727dbdf8ec6a2bdb26eb1
Kyla <kylalouisenel@gmail.com>
2022-03-11 23:31:20 +0000

    made some changes to base.js

---
+++ venyou/templates/venyou_app/base.js
@@ -0,0 +1,13 @@
+container.onmouseover = container.onmouseout = handler;
+
+function handler(event) {
+
+  log.value += event.type + ':  ' +
+    'target=' + str(event.target) +
+    ',  relatedTarget=' + str(event.relatedTarget) + "\n";
+  log.scrollTop = log.scrollHeight;
+
+  if (event.type == 'mouseover') {
+    event.target.style.background = 'pink';
+  }
+}
